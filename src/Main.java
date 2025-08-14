94f83aca126f07f10bc55959e183943d21b98438
Kuldeep-Tapodhan <djtapodhan143@gmail.com>
2025-08-14 18:30:53 +0530

    Main method overloading

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 public class Main {
     static  int a=5;
 
+
     public static void solve(){
         System.out.println(a);
     }
     public static void main(String[] args) {
-
+        main(23);
+        main("Deep Tapodhan");
+        main();
         solve();
+    }
+
+    public static void main() {
+        System.out.println("Hello There");
+    }
+
+    public static void main(String name) {
+        System.out.println(name);
+    }
+    public static void main(int age){
+        System.out.println(age);
     }
 
 
